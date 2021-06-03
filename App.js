b1ab0b21464dea45816b4a6498d89ce4b7443f30
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-06-03 12:54:34 +0300

    initialize firebase

--- App.js
+++ App.js
@@ -1,47 +1,68 @@
-import React from 'react';
-import Home from './screens/Home'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import ToDoList from './screens/ToDoList'
-import EditList from './screens/EditList'
-import Colors from './constans/Colors'
+import React from "react";
+import Home from "./screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import ToDoList from "./screens/ToDoList";
+import EditList from "./screens/EditList";
+import Colors from "./constans/Colors";
+import firebase from "firebase/app";
 
-let Stack = createStackNavigator()
+let Stack = createStackNavigator();
+let AuthStack = createStackNavigator();
+
+let Screens = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Fire App" component={Home} />
+      <Stack.Screen
+        component={ToDoList}
+        name="ToDoList"
+        options={({ route }) => {
+          return {
+            title: route.params.title,
+            headerStyle: {
+              backgroundColor: route.params.color,
+            },
+            headerTintColor: "white",
+          };
+        }}
+      />
+      <Stack.Screen
+        name="EditList"
+        component={EditList}
+        options={({ route }) => {
+          return {
+            title: route.params.title
+              ? `Edit ${route.params.title} list`
+              : "Create new List",
+            headerStyle: {
+              backgroundColor: route.params.color || Colors.blue,
+            },
+            headerTintColor: "white",
+          };
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
-  return (
+  return(
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Fire App' component={ Home } />
-        <Stack.Screen
-          component={ ToDoList }
-          name='ToDoList'
-          options={({ route }) => {
-            return({
-              title: route.params.title,
-              headerStyle: {
-                backgroundColor: route.params.color
-              },
-              headerTintColor: 'white',
-            })
-          }}
-        />
-        <Stack.Screen
-          name='EditList'
-          component={ EditList }
-          options={({ route }) => {
-            return({
-              title: route.params.title ? `Edit ${ route.params.title } list` : 'Create new List',
-              headerStyle: {
-                backgroundColor: route.params.color || Colors.blue
-              },
-              headerTintColor: 'white'
-            })
-          }}
-        />
-      </Stack.Navigator>
+      <Screens/>
     </NavigationContainer>
-  );
+  )
 }
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDHK3hXBzviFn8uBJSbbWGF71u5ocx8BFk",
+  authDomain: "todo-53285.firebaseapp.com",
+  projectId: "todo-53285",
+  storageBucket: "todo-53285.appspot.com",
+  messagingSenderId: "850891770501",
+  appId: "1:850891770501:web:69d851a7e8c0b6770b37ec",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
